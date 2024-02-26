209caaf09d373713ec1d94935c8a7d00b6d1af9a
soyalexokei <alejandrofernandezperez@protonmail.com>
2024-02-26 01:18:47 +0100

    47º commit

--- assets/js/carrusel1a.js
+++ assets/js/carrusel1a.js
@@ -5,37 +5,39 @@ window.onload = function() {
       { src: './assets/img/austria3.mp4', type: 'video/mp4' }
   ];
 
-  var videoIndex = 0;
+  var videoActual = 1;
+  const totalVideos = 3;
   var videoContainer = document.getElementById('carrusel-voluntariado-espec2');
 
   function playNextVideo(n) {
+    if(videoActual < 1) {
+      videoActual = totalVideos;
+    }else if(videoActual > totalVideos) {
+      videoActual = 1;
+    }
       // Crear elemento de video
       var video = document.createElement('video');
-      video.src = videos[videoIndex].src;
-      video.type = videos[videoIndex].type;
+      video.src = videos[videoActual-1].src;
+      video.type = videos[videoActual-1].type;
       video.controls = true;
       video.autoplay = true;
       video.width = 300;
       video.height = 400;
-
       // Limpiar el contenedor antes de agregar el nuevo video
       videoContainer.innerHTML = '';
       videoContainer.appendChild(video);
-
-      // Incrementar el índice para el próximo video
-      videoIndex = (videoIndex + 1) % videos.length;
   }
-
-  // Reproducir el primer video al cargar la página
-  playNextVideo(videoIndex);
 
   // Asociar la función playNextVideo con el clic en las flechas
   document.getElementById('flecha-izquierda-pase2').addEventListener('click', () => {
-    videoIndex--;
-    playNextVideo(videoIndex);
+    videoActual--;
+    playNextVideo(videoActual);
   });
   document.getElementById('flecha-derecha-pase2').addEventListener('click', () => {
-    videoIndex++;
-    playNextVideo(videoIndex);
+    videoActual++;
+    playNextVideo(videoActual);
   });
+
+  // Reproducir el primer video al cargar la página
+  playNextVideo(videoActual);
 };
