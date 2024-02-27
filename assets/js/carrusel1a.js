787cea877d6981adc98ba54a8c73af5714d53888
soyalexokei <alejandrofernandezperez@protonmail.com>
2024-02-27 16:58:29 +0100

    56º commit

--- assets/js/carrusel1a.js
+++ assets/js/carrusel1a.js
@@ -1,39 +1,47 @@
-const videoContainer1 = document.getElementById('carrusel-voluntariado-espec2');
-const flechaIzquierda2 = document.getElementById("flecha-izquierda-pase2");
-const flechaDerecha2 = document.getElementById("flecha-derecha-pase2");
+window.onload = function() {
+  var austria = [
+    {src: './assets/img/austria1.mp4', type: 'video/mp4'},
+    {src: './assets/img/austria2.mp4', type: 'video/mp4'},
+    {src: './assets/img/austria3.mp4', type: 'video/mp4'}
+  ];
 
-const totalVideos1 = 3;
-var videoActual1 = 1;
+  const videoContainer1 = document.getElementById('carrusel-voluntariado-espec2');
+  const flechaIzquierda2 = document.getElementById("flecha-izquierda-pase2");
+  const flechaDerecha2 = document.getElementById("flecha-derecha-pase2");
 
-function playNextVideo1(n) {
-  if(videoActual1 < 1) {
-    videoActual1 = totalVideos1;
-  }else if(videoActual1 > totalVideos1) {
-    videoActual1 = 1;
+  const totalVideos1 = 3;
+  var videoActual1 = 1;
+
+  function playNextVideo1(n) {
+    if(videoActual1 < 1) {
+      videoActual1 = totalVideos1;
+    }else if(videoActual1 > totalVideos1) {
+      videoActual1 = 1;
+    }
+    
+    //-- Crea video.
+    var video1 = document.createElement("video");
+    video1.src = austria[videoActual1-1].src;
+    video1.type = austria[videoActual1-1].type;
+    video1.controls = true;
+    video1.autoplay = false;
+    video1.width = 300;
+    video1.height = 400;
+
+    //-- Limpia el contenedor.
+    videoContainer1.innerHTML = '';
+    videoContainer1.appendChild(video1);
   }
 
-  //-- Limpia el contenedor.
-  videoContainer1.innerHTML = '';
+  flechaIzquierda2.addEventListener('click', () => {
+    videoActual1--;
+    playNextVideo1(videoActual1);
+  });
+  flechaDerecha2.addEventListener('click', () => {
+      videoActual1++;
+      playNextVideo1(videoActual1);
+  });
 
-  //-- Crea video.
-  const video1 = document.createElement("video");
-  video1.src = `./assets/img/austria${videoActual1}.mp4`;
-  video1.type = 'video/mp4';
-  video1.controls = true;
-  video1.autoplay = false;
-  video1.width = 300;
-  video1.height = 400;
-  videoContainer1.appendChild(video1);
-}
-
-flechaIzquierda2.addEventListener('click', () => {
-  videoActual1--;
+  // Reproducir el primer video al cargar la página
   playNextVideo1(videoActual1);
-});
-flechaDerecha2.addEventListener('click', () => {
-  videoActual1++;
-  playNextVideo1(videoActual1);
-});
-
-// Reproducir el primer video al cargar la página
-playNextVideo1(videoActual1);
+};
--- assets/js/carrusel2a.js
+++ assets/js/carrusel2a.js
@@ -1,39 +1,60 @@
-const videoContainer2 = document.getElementById('carrusel-voluntariado-espec4');
-const flechaIzquierda4 = document.getElementById("flecha-izquierda-pase4");
-const flechaDerecha4 = document.getElementById("flecha-derecha-pase4");
+window.onload = function() {
+    var alemania = [
+        {src: './assets/img/germany/germany1.mp4', type: 'video/mp4'},
+        {src: './assets/img/germany/germany2.mp4', type: 'video/mp4'},
+        {src: './assets/img/germany/germany3.mp4', type: 'video/mp4'},
+        {src: './assets/img/germany/germany4.mp4', type: 'video/mp4'},
+        {src: './assets/img/germany/germany5.mp4', type: 'video/mp4'},
+        {src: './assets/img/germany/germany6.mp4', type: 'video/mp4'},
+        {src: './assets/img/germany/germany7.mp4', type: 'video/mp4'},
+        {src: './assets/img/germany/germany8.mp4', type: 'video/mp4'},
+        {src: './assets/img/germany/germany9.mp4', type: 'video/mp4'},
+        {src: './assets/img/germany/germany10.mp4', type: 'video/mp4'},
+        {src: './assets/img/germany/germany11.mp4', type: 'video/mp4'},
+        {src: './assets/img/germany/germany12.mp4', type: 'video/mp4'},
+        {src: './assets/img/germany/germany13.mp4', type: 'video/mp4'},
+        {src: './assets/img/germany/germany14.mp4', type: 'video/mp4'},
+        {src: './assets/img/germany/germany15.mp4', type: 'video/mp4'},
+        {src: './assets/img/germany/germany16.mp4', type: 'video/mp4'},
+    ];
 
-const totalVideos2 = 4;
-var videoActual2 = 1;
+    const videoContainer2 = document.getElementById('carrusel-voluntariado-espec4');
+    const flechaIzquierda4 = document.getElementById("flecha-izquierda-pase4");
+    const flechaDerecha4 = document.getElementById("flecha-derecha-pase4");
 
-function playNextVideo2(p) {
-  if(videoActual2 < 1) {
-    videoActual2 = totalVideos2;
-  }else if(videoActual2 > totalVideos2) {
-    videoActual2 = 1;
-  }
-  
-  //-- Limpia el contenedor.
-  videoContainer2.innerHTML = '';
+    const totalVideos2 = 16;
+    var videoActual2 = 1;
 
-  //-- Crea video.
-  const video2 = document.createElement("video");
-  video2.src = `./assets/img/germany/germany${videoActual2}.mp4`;
-  video2.type = 'video/mp4';
-  video2.controls = true;
-  video2.autoplay = false;
-  video2.width = 300;
-  video2.height = 400;
-  videoContainer2.appendChild(video2);
-}
+    function playNextVideo2(p) {
+        if(videoActual2 < 1) {
+          videoActual2 = totalVideos2;
+        }else if(videoActual2 > totalVideos2) {
+          videoActual2 = 1;
+        }
+      
+        //-- Crea video.
+        var video2 = document.createElement("video");
+        video2.src = alemania[videoActual2-1].src;
+        video2.type = alemania[videoActual2-1].type;
+        video2.controls = true;
+        video2.autoplay = false;
+        video2.width = 300;
+        video2.height = 400;
 
-flechaIzquierda4.addEventListener('click', () => {
-  videoActual2--;
-  playNextVideo2(videoActual2);
-});
-flechaDerecha4.addEventListener('click', () => {
-  videoActual2++;
-  playNextVideo2(videoActual2);
-});
-
-//-- Primera entrada.
-playNextVideo2(videoActual2);
+        //-- Limpia el contenedor.
+        videoContainer2.innerHTML = '';
+        videoContainer2.appendChild(video2);
+    }
+      
+    flechaIzquierda4.addEventListener('click', () => {
+        videoActual2--;
+        playNextVideo2(videoActual2);
+    });
+    flechaDerecha4.addEventListener('click', () => {
+        videoActual2++;
+        playNextVideo2(videoActual2);
+    });
+    
+    //-- Primera entrada.
+    playNextVideo2(videoActual2);
+};
