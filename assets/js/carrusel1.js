7dd46872cd0b11ec75b4b39b78a142d0aec4b1c2
soyalexokei <alejandrofernandezperez@protonmail.com>
2024-09-15 19:16:53 +0200

    150º commit

--- assets/js/carrusel1.js
+++ assets/js/carrusel1.js
@@ -1,29 +1,62 @@
-const pantalla1 = document.getElementById("pantalla-voluntariado-proweb-1");
+//-- Variables.
+var videosIDvoluntariado1 = ["1","2","3","4","5","6","7","8","9","10",
+"11","eyGhyAdtc30","13","14","f0PHhWc2fb8","16","17","18","19","20",
+"21","22","Y38JfytAGp4","24","25","26","27","28","29","30",
+"31","32","33","34","35","36","37","38","39","40","41"];
+var indiceVoluntariado1 = 1;
+var pantallaVoluntariado1 = document.getElementById("pantalla-voluntariado-proweb-1");
+var imgVoluntariado1 = document.createElement("img");
+var iframeVoluntariado1 = document.createElement("iframe");
+let auxImgVoluntariado1 = new Image();
 
-const totalImagenes1 = 38;
-let imagenActual1 = 1;
+//-- Ctes.
+const totalVoluntariado1 = 41;
 
-function cambiarImagen1(m) {
-  if (imagenActual1 < 1) {
-    imagenActual1 = totalImagenes1;
-  } else if (imagenActual1 > totalImagenes1) {
-    imagenActual1 = 1;
-  }
-  pantalla1.style.transform = `translateX(-${(imagenActual1 - 1) * 100}%)`;
+function mostrarVoluntariado1(auxVoluntariado1) {
+    
+    //-- Limpiar la pantalla.
+    pantallaVoluntariado1.innerHTML = "";
+
+    //-- Verificar si es una imagen o un vídeo.
+    auxImgVoluntariado1.src = `./assets/img/voluntariados/Segundo/${auxVoluntariado1}.jpg`;
+    auxImgVoluntariado1.onload = function() {
+      imgVoluntariado1.src = auxImgVoluntariado1.src;
+        pantallaVoluntariado1.appendChild(imgVoluntariado1);
+    }
+    auxImgVoluntariado1.onerror = function() {
+      iframeVoluntariado1.src = "https://www.youtube.com/embed/" + videosIDvoluntariado1[auxVoluntariado1-1] + "?autoplay=1&loop=1&playlist=" + videosIDvoluntariado1[auxVoluntariado1-1];
+      iframeVoluntariado1.allow = "autoplay";
+      iframeVoluntariado1.width = "400";
+      iframeVoluntariado1.height = "533";
+      iframeVoluntariado1.allowFullscreen = false;
+      iframeVoluntariado1.rel = 0;
+      iframeVoluntariado1.controls = 0;
+      pantallaVoluntariado1.appendChild(iframeVoluntariado1);
+    }
 }
 
-document.getElementById("flecha-izquierda-1").addEventListener("click", () => {
-  imagenActual1--;
-  cambiarImagen1(imagenActual1);
-});
-
-document.getElementById("flecha-derecha-1").addEventListener("click", () => {
-  imagenActual1++;
-  cambiarImagen1(imagenActual1);
-});
-
-for (let i = 1; i <= totalImagenes1; i++) {
-  const imagen1 = document.createElement("img");
-  imagen1.src = `./assets/img/voluntariados/Segundo/austria${i}.jpg`;
-  pantalla1.appendChild(imagen1);
+function imgAnteriorVoluntariado1() {
+    if(indiceVoluntariado1 > 1) {
+      indiceVoluntariado1--;
+    }else {
+      indiceVoluntariado1 = totalVoluntariado1;
+    }
+    mostrarVoluntariado1(indiceVoluntariado1);
 }
+
+function imgSiguienteVoluntariado1() {
+    if(indiceVoluntariado1 < totalVoluntariado1) {
+      indiceVoluntariado1++;
+    }else {
+      indiceVoluntariado1 = 1;
+    }
+    mostrarVoluntariado1(indiceVoluntariado1);
+}
+
+//-- Pulsar flecha izquierda.
+document.getElementById("flecha-izquierda-1").addEventListener("click", imgAnteriorVoluntariado1);
+//-- Pulsar flecha derecha.
+document.getElementById("flecha-derecha-1").addEventListener("click", imgSiguienteVoluntariado1);
+
+//-- Punto de inicio del programa.
+mostrarVoluntariado1(indiceVoluntariado1);
