0a168a22040f865db8f5a7eae26a5b3963742003
soyalexokei <alejandrofernandezperez@protonmail.com>
2024-10-15 13:06:56 +0200

    189º commit

--- assets/documentos/Formaciones/AIcourse/windows.js
+++ assets/documentos/Formaciones/AIcourse/windows.js
@@ -1,12 +1,15 @@
 //-- Variables.
 var btnModulo1 = document.getElementById("btnMod1");
 var btnModulo2 = document.getElementById("btnMod2");
+var btnModulo3 = document.getElementById("btnMod3");
 var contenidoModulo1 = document.getElementById("contenidoModulo1");
 var contenidoModulo2 = document.getElementById("contenidoModulo2");
+var contenidoModulo3 = document.getElementById("contenidoModulo3");
 
 //-- Ocultarlo inicialmente.
 contenidoModulo1.classList.add("oculto");
 contenidoModulo2.classList.add("oculto");
+contenidoModulo3.classList.add("oculto");
 
 //-- Acción de ocultar/desplegar contenido.
 function actionIA(contenido) {
@@ -19,4 +22,7 @@ btnModulo1.addEventListener("click", () => {
 });
 btnModulo2.addEventListener("click", () => {
     actionIA(contenidoModulo2);
+});
+btnModulo3.addEventListener("click", () => {
+    actionIA(contenidoModulo3);
 });
