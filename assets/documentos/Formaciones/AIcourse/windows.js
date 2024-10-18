52b4b0bc4713ebafbe094584a65242857a2b571c
soyalexokei <alejandrofernandezperez@protonmail.com>
2024-10-18 19:36:59 +0200

    194º commit

--- assets/documentos/Formaciones/AIcourse/windows.js
+++ assets/documentos/Formaciones/AIcourse/windows.js
@@ -2,14 +2,17 @@
 var btnModulo1 = document.getElementById("btnMod1");
 var btnModulo2 = document.getElementById("btnMod2");
 var btnModulo3 = document.getElementById("btnMod3");
+var btnEjerModulo3 = document.getElementById("btnEjerMod3");
 var contenidoModulo1 = document.getElementById("contenidoModulo1");
 var contenidoModulo2 = document.getElementById("contenidoModulo2");
 var contenidoModulo3 = document.getElementById("contenidoModulo3");
+var contenidoEjerModulo3 = document.getElementById("contenidoEjerModulo3");
 
 //-- Ocultarlo inicialmente.
 contenidoModulo1.classList.add("oculto");
 contenidoModulo2.classList.add("oculto");
 contenidoModulo3.classList.add("oculto");
+contenidoEjerModulo3.classList.add("oculto");
 
 //-- Acción de ocultar/desplegar contenido.
 function actionIA(contenido) {
@@ -25,4 +28,7 @@ btnModulo2.addEventListener("click", () => {
 });
 btnModulo3.addEventListener("click", () => {
     actionIA(contenidoModulo3);
+});
+btnEjerModulo3.addEventListener("click", () => {
+    actionIA(contenidoEjerModulo3);
 });
