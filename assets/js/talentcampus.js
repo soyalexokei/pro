5303b07d90d3c3c7f6b9c6d89534ac107ab12b83
soyalexokei <alejandrofernandezperez@protonmail.com>
2025-06-06 00:50:16 +0200

    318 commit

---
+++ assets/js/talentcampus.js
@@ -0,0 +1,8 @@
+//-- Variables.
+var imagennotastalent = "assets/experiencias/PSS-TI/notas-Talent.png";
+var pantallanotastalent = document.getElementById('pantalla-notastalent');
+var imgnotastalent = document.createElement("img");
+
+//-- Programa.
+imgnotastalent.src = imagennotastalent;
+pantallanotastalent.appendChild(imgnotastalent);
