aaf9d8d9875f3b173251e95377825640d7e06e41
soyalexokei <alejandrofernandezperez@protonmail.com>
2025-07-25 11:38:20 +0200

    339 commit

--- assets/habilidades/deportivas/js/monitorExp.js
+++ assets/habilidades/deportivas/js/monitorExp.js
@@ -38,7 +38,7 @@ function mostrarMonitorExp(auxMonitorExp) {
         <p><b>Puesto:</b> Monitor de Ocio y Tiempo Libre</p>
         <p><b>Lugar:</b> <a class="otros-enlaces" href="https://site.educa.madrid.org/cp.angelgonzalez.leganes/" target="_blank">CEIP Ángel González</a> de Leganés, Madrid, España</p>
         <p><b>Sector:</b> Educación y tiempo libre</p>
-        <p><b>Tipo:</b> Campamento urbano de verano (sin pernocta) para niños/as<br>entre 4 años y 12 años</p>
+        <p><b>Tipo:</b> Campamento urbano de verano (sin pernocta) para<br>niños/as entre 4 años y 12 años</p>
         <p><b>Tipo de contrato:</b> Contrato por hora</p>
         <p><b>Funciones:</b></p>
         <p> - Organización y puesta en marcha de actividades<br>lúdico educativas, deportivas, competitivas y<br>cooperativas</p>
@@ -50,8 +50,8 @@ function mostrarMonitorExp(auxMonitorExp) {
         <p> - Liderar el grupo de niños/as asignado</p>
         <p><b>Teléfono:</b> 91 591 49 54</p>
         <p><b>Correo electrónico:</b> info@dnmo.es</p>
-        <p><b>Sede de la empresa:</b> Calle de Mercedes Rodríguez, 11, Madrid 28044,<br>Madrid, España</p>
-        <p><b>Horario de atención al público:</b> de Lunes a Viernes de 09:00 a 17:00</p>
+        <p><b>Sede de la empresa:</b> Calle de Mercedes Rodríguez, 11,<br>Madrid 28044, Madrid, España</p>
+        <p><b>Horario de atención al público:</b> de Lunes a Viernes de<br>09:00 a 17:00</p>
       </div>
     `;
   }else if(auxMonitorExp === 7) {
@@ -66,19 +66,19 @@ function mostrarMonitorExp(auxMonitorExp) {
         <p><b>Puesto:</b> Monitor de Ocio y Tiempo Libre</p>
         <p><b>Lugar:</b> <a class="otros-enlaces" href="https://www.instagram.com/bordafelices/" target="_blank">Broto, Huesca, Aragón, España</a></p>
         <p><b>Sector:</b> Educación y tiempo libre</p>
-        <p><b>Tipo:</b> Campamento de verano con pernocta para chavales/as<br>entre 12 años y 17 años</p>
+        <p><b>Tipo:</b> Campamento de verano con pernocta para<br>chavales/as entre 12 años y 17 años</p>
         <p><b>Tipo de contrato:</b> Contrato por hora</p>
         <p><b>Funciones:</b></p>
         <p> - Organización y puesta en marcha de actividades<br>lúdico educativas, deportivas, competitivas y<br>cooperativas</p>
-        <p> - Organización y puesta en marcha de las actividades a realizar<br>cada día</p>
+        <p> - Organización y puesta en marcha de las actividades a<br>realizar cada día</p>
         <p> - Resolución de conflictos que surjan</p>
-        <p> - Trabajo diario con los participantes del campamento, incluso<br>control durante la noche</p>
-        <p> - Organización y puesta en marcha de excursiones por las cercanías<br>de Broto: Monte Perdido y el Valle de Ordesa</p>
-        <p> - Enseñanza de valores a los participantes del campamento</p>
-        <p> - Coliderar el grupo genérico, junto a los compañeros/as monitores</p>
+        <p> - Trabajo diario con los participantes del campamento,<br>incluso control durante la noche</p>
+        <p> - Organización y puesta en marcha de excursiones por<br>las cercanías de Broto: Monte Perdido<br>y el Valle de Ordesa</p>
+        <p> - Enseñanza de valores a los participantes del<br>campamento</p>
+        <p> - Coliderar el grupo genérico, junto a los compañeros/as<br>monitores</p>
         <p><b>Teléfono:</b> 618 46 01 64</p>
         <p><b>Correo electrónico:</b> encantaria@encantaria.com</p>
-        <p><b>Sede central de la empresa:</b> Calle de Cabañera Real, 7, Fraga 22520,<br>Huesca, Aragón, España</p>
+        <p><b>Sede central de la empresa:</b> Calle de Cabañera Real, 7,<br>Fraga 22520, Huesca, Aragón, España</p>
       </div>
     `;
   }else {
