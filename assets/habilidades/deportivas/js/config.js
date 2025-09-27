8c390e85a4492a37f9f6292cb7dc629ec76d4c4e
soyalexokei <alejandrofernandezperez@protonmail.com>
2025-09-27 17:36:32 +0200

    351 commit

---
+++ assets/habilidades/deportivas/js/config.js
@@ -0,0 +1,36 @@
+const p_totales = document.getElementById("p_totales")
+const victorias = document.getElementById("victorias")
+const derrotas = document.getElementById("derrotas")
+const empates = document.getElementById("empates")
+const ctx = document.getElementById('performanceChart').getContext('2d');
+var eficacia = document.getElementById("eficacia")
+const nivel = document.getElementById("nivel")
+const fiabilidad = document.getElementById("fiabilidad")
+
+p_totales.innerHTML = 84
+victorias.innerHTML = 41
+derrotas.innerHTML = 32
+empates.innerHTML = 11
+nivel.innerHTML = 2.57
+fiabilidad.innerHTML = "Alta - " + 83.90 + "%"
+
+const partidos_totales = parseInt(p_totales.innerHTML)
+const partidos_ganados = parseInt(victorias.innerHTML)
+const partidos_perdidos = parseInt(derrotas.innerHTML)
+const partidos_empatados = parseInt(empates.innerHTML)
+
+//-- Cálculo de la Eficacia.
+eficacia.innerHTML = Math.round((partidos_ganados * 100) / partidos_totales) + "%"
+
+new Chart(ctx, {
+    type: 'pie',
+    data: {
+        datasets: [{
+            data: [partidos_ganados, partidos_perdidos, partidos_empatados],
+            backgroundColor: ['#34D399', '#EF4444', '#FFEB3B'],
+        }]
+    },
+    options: {
+        responsive: true
+    }
+});
